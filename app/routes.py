62cf65bf707dc6c80c29b9d2182afbabd1b73a7d
isma <ismaelmed500@gmail.com>
2024-11-14 15:30:48 -0300

    arreglo de las rutas

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,11 @@
 import os
 from flask import render_template
 from app import app
-import markdown
 
 @app.route('/')
 def index():
-    # La ruta ahora es a la raíz del proyecto
-    readme_path = os.path.join(os.getcwd(), 'README.md')
+    return render_template('index.html')
 
-    try:
-        with open(readme_path, 'r', encoding='utf-8') as f:
-            readme_content = f.read()
-    except FileNotFoundError:
-        return "El archivo README.md no se encuentra."
-
-    # Convertir el contenido Markdown a HTML
-    html_content = markdown.markdown(readme_content)
-
-    # Pasar el HTML generado a la plantilla
-    return render_template('index.html', readme_html=html_content)
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app  # Importa la instancia de app desde __init__.py
+from app import app  
 
 if __name__ == "__main__":
     app.run(debug=True)
